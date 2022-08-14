1a9c857b64cf2e3e25b98027202bc7d129017865
Thibault RICHARD <thibault@widop.com>
2022-08-14 18:25:36 +0200

    Full reproducer

--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
 import Chart from 'chart.js/auto';
 
-const chart = new Chart(canvasContext, payload);
+const chart = new Chart('myChart', {
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+    }
+});
+
+console.log(chart);
